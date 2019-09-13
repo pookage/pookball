a6c75d0289028bfb17cb0ee30db12c4448fce772
Pookage <ahoy@pookage.dev>
2019-09-13 21:45:06 +0100

    abstracted the goal drawing into its own function

--- src/components/Canvas/utils.js
+++ src/components/Canvas/utils.js
@@ -6,25 +6,32 @@ export function draw(canvas){
 	const context = canvas.getContext("2d");
 	
 	// dimensions
+	// ------------------------------
 	const { width, height } = canvas;
 	const playerSize        = height * playerScale;
+
+
+	//scope binding
+	// ------------------------------
 	const drawPlayer        = player.bind(true, context, playerSize);
+	const drawGoal          = goal.bind(true, context, playerSize);
+	const drawGuide         = guide.bind(true, context);
+
 
 	// positioning
+	// ------------------------------
 	const xCenter       = width / 2;
 	const yCenter       = height / 2;
 
+
 	// draw guides
 	// -------------------------------
-	const drawGuide = guide.bind(true, context);
-
 	// vertical
 	drawGuide({
 		start: [ xCenter, 0 ], 
 		end: [ xCenter, height ], 
 		color: "red"
-	});
-	
+	});	
 	// horizontal
 	drawGuide({
 		start: [ 0, yCenter ],
@@ -33,32 +40,17 @@ export function draw(canvas){
 	});
 
 
+	// draw goal
+	// ---------------------------
+	drawGoal({ x: xCenter, y: 0 });
+
+
 	// draw players
 	// ----------------------------
 	// striker
 	drawPlayer({ x: xCenter, y: yCenter });
-
-
-
-	// draw goal
-	// ---------------------------
-	const goalWidth  = playerSize * 10;
-	const goalRadius = goalWidth / 2;
-	const goalHeight = goalWidth * 0.05;
-	const xGoalStart = xCenter - goalRadius;
-	const xGoalEnd   = xCenter + goalRadius;
-	const yGoalStart = 0
-	const yGoalEnd   = goalHeight;
-	const goalieOffset = goalHeight + (playerSize * 0.1) + (playerSize / 2);
-
-	// context.beginPath();
-	context.fillStyle = "green";
-	context.fillRect(
-		xGoalStart, yGoalStart, 
-		goalWidth, goalHeight
-	);
-
-	drawPlayer({ x: xCenter, y: goalieOffset });
+	// goalie
+	drawPlayer({ x: xCenter, y: playerSize * 1.1 });	
 
 }// draw
 
@@ -88,3 +80,22 @@ function guide(context, { start, end, color }){
 	context.lineTo(endX, endY);
 	context.stroke();
 }// drawGuide
+
+function goal(context, playerSize, { x, y }){
+
+	const goalWidth  = playerSize * 10;
+	const goalRadius = goalWidth / 2;
+	const goalHeight = goalWidth * 0.05;
+	const xGoalStart = x - goalRadius;
+	const xGoalEnd   = x + goalRadius;
+	const yGoalStart = y
+	const yGoalEnd   = goalHeight;
+	const goalieOffset = goalHeight + (playerSize * 0.1) + (playerSize / 2);
+
+	// context.beginPath();
+	context.fillStyle = "green";
+	context.fillRect(
+		xGoalStart, yGoalStart, 
+		goalWidth, goalHeight
+	);
+}// goal
